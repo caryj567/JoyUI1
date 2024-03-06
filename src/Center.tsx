d19256e0ff8561654e7e681fe8f376b48ac3e60f
caryj567 <157739833+caryj567@users.noreply.github.com>
2024-03-06 10:31:45 -0500

    Update Center.tsx

--- src/Center.tsx
+++ src/Center.tsx
@@ -14,29 +14,22 @@ export default function BasicButtons()
     return (
     
         <Box>
-        <Sheet>
-      <FormControl
-        id="Id"
-        required
-        size="sm"
-        color="primary">
-        <FormLabel>
-          Label
-        </FormLabel>
-        <Input
-          placeholder="Placeholder"
-          name="Name"
-          type="tel"
-          autoComplete="on"
-          autoFocus
-          error
-          fullWidth
-          defaultValue="DefaultValue"
-          variant="outlined" />
-        <FormHelperText>
-          Help!
-        </FormHelperText>
-      </FormControl>
+            <Sheet>
+                <Typography>Fuck you</Typography>
+              <FormControl id="Id" required size="sm" color="primary">
+                <FormLabel>Label</FormLabel>
+                <Input
+                  placeholder="Placeholder"
+                  name="Name"
+                  type="tel"
+                  autoComplete="on"
+                  autoFocus
+                  error
+                  fullWidth
+                  defaultValue="DefaultValue"
+                  variant="outlined" />
+                <FormHelperText>Help!</FormHelperText>
+              </FormControl>
             </Sheet>
         </Box>
     
