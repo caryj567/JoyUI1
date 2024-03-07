99fab9d86763555cbb1e37afecd80a9f64f3b441
caryj567 <157739833+caryj567@users.noreply.github.com>
2024-03-07 14:06:19 -0500

    Update component2.tsx

--- src/component2.tsx
+++ src/component2.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/joy/Box';
 import Slider from '@mui/joy/Slider';
+import Button from '@mui/joy/Button';
 
 function valueText(value: number) {
   return `${value}°C`;
@@ -9,7 +10,10 @@ function valueText(value: number) {
 export default function StepsSlider() {
   return (
     <Box sx={{ width: 300 }}>
-      <Slider
+      <Button>
+        In component two
+      </Button>
+      {/* <Slider
         aria-label="Small steps"
         defaultValue={0.00000005}
         getAriaValueText={valueText}
@@ -18,7 +22,7 @@ export default function StepsSlider() {
         min={-0.00000005}
         max={0.0000001}
         valueLabelDisplay="auto"
-      />
+      /> */}
     </Box>
   );
 }
