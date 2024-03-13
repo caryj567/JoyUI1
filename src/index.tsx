10ea93f8d793a238a6c4627965e8167b753fce57
caryj567 <157739833+caryj567@users.noreply.github.com>
2024-03-13 12:38:01 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
 import Demo from './Demo';
 import Center from './Center';
+import Login from './Login';
 import Feedback from './Feedback';
 import StepsSlider from './component2';
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.querySelector("#root")!).render(
       <CssVarsProvider>
         {/*<Demo />*/}
          <Center />
+        <Login />
         <Feedback />
         <StepsSlider />
       </CssVarsProvider>
