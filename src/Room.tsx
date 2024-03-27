6a141886550ee19cc51493da557c4749896f9fd1
caryj567 <157739833+caryj567@users.noreply.github.com>
2024-03-27 10:40:23 -0400

    Update Room.tsx

--- src/Room.tsx
+++ src/Room.tsx
@@ -14,8 +14,9 @@ export default function BasicButtons()
     return (
     
         <Box alignItems="center">
-            <Sheet color="primary" variant="soft"  sx={{width: 350, my:"auto"}}>
+            <Sheet color="primary" variant="soft"  sx={{width: 350, mx:"auto" my:"auto"}}>
                 <Typography level="h1" sx={{py:2}}>Create Room</Typography>
+                <div style={{display:'flex', alignItems:'right', justifyContent:'right'}}>
                 <FormControl id="Id" required size="sm" color="primary">
                     <Typography sx={{mx:"auto", py:2}}>Room name</Typography>
                     <Input
@@ -31,6 +32,7 @@ export default function BasicButtons()
                       variant="outlined" />
                     {/*<FormHelperText>Help!</FormHelperText>*/}
                   </FormControl>
+                    </div>
                 <div style={{display:'flex', alignItems:'right', justifyContent:'right'}}>
                     <Button sx={{width:100, mx:"auto", py:2, px: 3}}>Create</Button>
                 </div>
