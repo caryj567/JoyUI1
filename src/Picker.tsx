5180cdc0a31d5715bdb093e23f38f7166d492018
caryj567 <157739833+caryj567@users.noreply.github.com>
2024-03-27 11:48:47 -0400

    Update Picker.tsx

--- src/Picker.tsx
+++ src/Picker.tsx
@@ -15,22 +15,8 @@ export default function BasicButtons()
     
         <Box alignItems="center">
             <Sheet color="primary" variant="soft" sx={{width: 350, mx:"auto", my:"auto"}}>
-                <Typography level="h4" sx={{mx:"auto", py:2}}>Welcome to BoardX</Typography>
-                <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
-                    <FormControl id="Id" required size="sm" color="primary" style={{width:250}}>
-                        <Input placeholder="Email" name="Name" type="tel" autoComplete="on" autoFocus fullWidth defaultValue="" variant="outlined" />
-                      </FormControl>
-                </div>
-                <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
-                    <FormControl id="Id" required size="sm" color="primary" style={{width:250}}>
-                        <Input placeholder="Password" name="Name" type="tel" autoComplete="on" autoFocus fullWidth defaultValue="" variant="outlined" />
-                      </FormControl>
-                </div>
                 <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
                     <Button sx={{width:250, mx:"auto", py:2, px: 3}}>Log in</Button>
-                </div>
-                <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
-                    <Typography sx={{mx:"auto", py:2}}>Forgot Password</Typography>
                 </div>
             </Sheet>
         </Box>
