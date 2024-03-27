4418f12339b877021e7b7581e5a94ebc492bc111
caryj567 <157739833+caryj567@users.noreply.github.com>
2024-03-27 12:10:38 -0400

    Update Picker.tsx

--- src/Picker.tsx
+++ src/Picker.tsx
@@ -17,7 +17,7 @@ export default function BasicButtons()
         <Box alignItems="center">
             <Sheet color="primary" variant="soft" sx={{width: 350, mx:"auto", my:"auto"}}>
                 <div style={{display:'flex', alignItems:'left', justifyContent:'left'}}>
-                    <Button sx={{width:"auto", mx:"auto", py:0, px: 0}}>Menu</Button>
+                    <Button sx={{width:"auto", mx:"auto", py:0, px: 0}} onClick={() => setPopUpMenu(!popUpMenu)} >Menu</Button>
                     <Button sx={{width:"auto", mx:"auto", py:0, px: 0}}>size1</Button>
                     <Button sx={{width:"auto", mx:"auto", py:0, px: 0}}>size2</Button>
                     <Button sx={{width:"auto", mx:"auto", py:0, px: 0}}>size3</Button>
