fa965deed201fa732aa7d206dc1c581af88c95d3
caryj567 <157739833+caryj567@users.noreply.github.com>
2024-04-01 13:38:32 -0400

    Update Center.tsx

--- src/Center.tsx
+++ src/Center.tsx
@@ -13,7 +13,7 @@ export default function BasicButtons()
 {
     return (
     
-        <Box alignItems="center">
+        <Box alignItems="Primary">
             <Sheet color="default" variant="soft" sx={{width: 350, mx:"auto", my:"auto"}}>
                 <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
                     <Typography level="h1" sx={{mx:"auto", py:2}}>Create Team</Typography>
